f9490542f164c48d51efe37d603ce6ae31da5c4a
idk <idk@fireaxe.local>
2017-05-14 04:16:30 -0400

    fixed a bunch of minor bugs and obsolescing debian.sh script

--- debian.sh
+++ debian.sh
@@ -5,8 +5,11 @@ SOURCEBIN=scpage.sh
 SOURCEDOC=README.md
 DEBFOLDER=scpage
 WRAP=scpage-wrap
-WRAP_HTML=scpage-html
-DEBVERSION=$(date +%Y%m%d)
+PAGE_EXAMPLE="scpage.html"
+INDEX_EXAMPLE="index.html"
+CSS_EXAMPLE="style.css"
+LICENSE="LICENSE"
+DEBVERSION=1.0
 CONTROL_FILE="Source: scpage
 Section: admin
 Priority: optional
@@ -21,7 +24,7 @@ Package: scpage
 Architecture: all
 Depends: \${misc:Depends}, markdown | discount
 Description: Generates a Markdown-formatted description of a software package
- and a corresponding html page. For use with my repo generator, apt-git so that
+ and a corresponding html page. For use with my repo generator, apt-now so that
  it can generate package description pages automatically and make the
  repositories easier to browse. It's a very, very basic little script. I don't
  intend to expand it or make it smarter, instead I'm going to use it by
@@ -39,6 +42,10 @@ mkdir $DEBFOLDERNAME
 # Copy your script to the source dir
 cp $SOURCEBINPATH/$SOURCEBIN $DEBFOLDERNAME/$DEBFOLDER
 cp $SOURCEBINPATH/$WRAP $DEBFOLDERNAME/$WRAP
+cp $SOURCEBINPATH/$PAGE_EXAMPLE $DEBFOLDERNAME/
+cp $SOURCEBINPATH/$INDEX_EXAMPLE $DEBFOLDERNAME/
+cp $SOURCEBINPATH/$CSS_EXAMPLE $DEBFOLDERNAME/
+cp $SOURCEBINPATH/$LICENSE $DEBFOLDERNAME/
 cd $DEBFOLDERNAME
 
 # Create the packaging skeleton (debian/*)
@@ -53,6 +60,10 @@ mv debian/rules.new debian/rules
 # as well as the target directory
 echo $DEBFOLDER usr/bin > debian/install
 echo $WRAP usr/bin >> debian/install
+echo $PAGE_EXAMPLE usr/share/doc/scpage >> debian/install
+echo $INDEX_EXAMPLE usr/share/doc/scpage >> debian/install
+echo $CSS_EXAMPLE usr/share/doc/scpage >> debian/install
+echo $LICENSE usr/share/doc/scpage >> debian/install
 #echo $SOURCEDOC usr/share/doc/apt-git >> debian/install
 
 # Remove the example files
